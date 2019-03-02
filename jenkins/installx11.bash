98d8642b69d671240f2d6389b4ce29cd5174caf8
Esteban2812 <ealvinoq@gmail.com>
2019-03-02 01:43:47 +0000

    allow ec2 to be accessible trough x11

---
+++ jenkins/installx11.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum install -y gcc-c++
+sudo yum install -y libX11-devel
+sudo yum install -y fontconfig-devel
+sudo yum install -y libXcursor-devel
+sudo yum install -y libXext-devel
+sudo yum install -y libXfixes
+sudo yum install -y libXft-devel
+sudo yum install -y libXi-devel
+sudo yum install -y libXrandr-devel
+sudo yum install -y libXrender-devel
+sudo yum install -y xorg-x11-xauth
+
